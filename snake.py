95a371142da2e981fe2773402c8d9eef2e618bfc
ErnestoGarciaArriola <A01706777@tec.mx>
2023-09-14 22:56:54 -0600

    Version probada por flake8 y corregida

--- snake.py
+++ snake.py
@@ -8,9 +8,8 @@ Exercises
 4. Change the snake to respond to mouse clicks.
 """
 
-from random import randrange
-from turtle import *
-
+import random
+import turtle
 from freegames import square, vector
 
 food = vector(0, 0)
@@ -36,35 +35,40 @@ def move():
 
     if not inside(head) or head in snake:
         square(head.x, head.y, 9, 'red')
-        update()
+        turtle.update()
         return
 
     snake.append(head)
 
     if head == food:
         print('Snake:', len(snake))
-        food.x = randrange(-15, 15) * 10
-        food.y = randrange(-15, 15) * 10
+        food.x = random.randrange(-15, 15) * 10
+        food.y = random.randrange(-15, 15) * 10
     else:
         snake.pop(0)
 
-    clear()
+    turtle.clear()
 
     for body in snake:
         square(body.x, body.y, 9, 'black')
 
     square(food.x, food.y, 9, 'green')
-    update()
-    ontimer(move, 100)
+    turtle.update()
+    turtle.ontimer(move, 100)
 
 
-setup(420, 420, 370, 0)
-hideturtle()
-tracer(False)
-listen()
-onkey(lambda: change(10, 0), 'Right')
-onkey(lambda: change(-10, 0), 'Left')
-onkey(lambda: change(0, 10), 'Up')
-onkey(lambda: change(0, -10), 'Down')
+# Configurar la ventana de Turtle
+turtle.setup(420, 420)
+turtle.hideturtle()
+turtle.tracer(False)
+
+turtle.listen()
+turtle.onkey(lambda: change(10, 0), 'Right')
+turtle.onkey(lambda: change(-10, 0), 'Left')
+turtle.onkey(lambda: change(0, 10), 'Up')
+turtle.onkey(lambda: change(0, -10), 'Down')
+
 move()
-done()
+
+# Mantener la ventana abierta
+turtle.done()
